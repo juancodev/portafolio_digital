607182eedd38a7313a530cf7e1ff3585c6093b1a
JUANCMS <montillasanchezjuancarlos@gmail.com>
2020-11-15 21:18:33 -0400

    Solución del Font Awesome y clase 4.15 terminada

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -1,26 +1,13 @@
 const yo = require('yo-yo');
-//EN ESTE CASO NO VAMOS A REQUERIR LA FUNCIÓN landing.
+//EN ESTE CASO NO VAMOS A REQUERIR LA FUNCIÓN landing SINO LA FUNCIÓN layaout.
+const layaout = require('../layout');
 
-//REVISAR ERROR EN EL LOGO DEL FONT-AWESOME!!!!!!! CLASE 4.15
-
-let template = yo `<nav class="header">
-  <div class="nav-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col s12 m6 offset-m1">
-          <a href="/" class="brand-logo portafolio">Portafolio Digital</a>
-        </div>
-        <div class="col s2 m6 push-m10">
-          <a href="#" class="btn btn-large btn-flat dropdown-button" data-activates="drop-user">
-          <i class="fas fa-user-circle"></i>
-          </a>
-          <ul id="drop-user" class="dropdown-content">
-            <li><a href="#">Salir</a></li>
-          </ul>
-        </div>
-      </div>
+let template = yo `<div class="container timeline">
+  <div class="row">
+    <div class="col s12 m10 offset-m1 l6 offset-l3">
+    content
     </div>
   </div>
-</nav>`;
+</div>`;
 
-module.exports = template;
+module.exports = layaout(template);
--- src/landing/index.js
+++ src/landing/index.js
@@ -2,7 +2,7 @@ let yo = require('yo-yo');
 
 //NOS RETORNA EL ELEMENTO DEL DOM QUE NOS PROCESA yo
 module.exports = function landing(box){
-  return yo`<div class="container">
+  return yo`<div class="container landing">
       <div class="row">
         <div class="col s10 push-s1">
           <div class="row">
