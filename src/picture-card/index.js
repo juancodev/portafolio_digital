c82cf55e71ff930b54e44b3ca74186e0b245f9e6
JUANCMS <montillasanchezjuancarlos@gmail.com>
2020-11-18 15:55:22 -0400

    Descarga de los pickture-card

---
+++ src/picture-card/index.js
@@ -0,0 +1,10 @@
+let yo = require('yo-yo');
+
+module.exports = yo`<div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="images/office.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title</span>
+    </div>
+  </div>`
