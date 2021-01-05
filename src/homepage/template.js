4259883d605ddc73678d51e0d3c461f762a331d2
JUANCMS <montillasanchezjuancarlos@gmail.com>
2021-01-05 14:45:31 -0400

    Actualización

--- src/homepage/template.js
+++ src/homepage/template.js
@@ -10,11 +10,11 @@ module.exports = function pictureCard (pictures) {
     <div class="row">
       <div class="col s12 m10 offset-m1 l8 offset-l2 center-align">
         <form enctype="multipart/form-data" class="form-upload" id="formUpload" onsubmit=${onsubmit}>
-          <div id="fileName" class="fileUpload btn btn-flat cyan">
+          <div id="fileName" class="fileUpload btn btn-flat blue darken-4">
             <span><i class="fas fa-camera-retro"></i> Subir una foto</span>
             <input name="picture" id="file" type="file" class="upload" onchange=${onchange}/>
           </div>
-          <button id="btnUpload" type="submit" class="btn btn-flat cyan hide">SUBIR</button>
+          <button id="btnUpload" type="submit" class="btn btn-flat blue darken-4 hide">SUBIR</button>
           <button id="btnCancel" type="button" class="btn btn-flat red hide" onclick="${cancel}"><i class="fas fa-times-circle"></i></button>
         </form>
       </div>
